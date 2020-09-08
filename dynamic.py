94f40e7167bec65bd1b0accea297ceb4eba72db2
AVINASH MADDIKONDA <SvasssakAvi@gmail.com>
2020-09-08 18:31:17 +0530

    Update dynamic.py
    Use `getpass.getuser` to get username, use ternary operations, remove unwanted type conversions and repeated calls, replace string concatenation with string interpolation, change folder name from *mojave* to *WallpaperChanger* to remove errors and inconsistencies

--- dynamic.py
+++ dynamic.py
@@ -1,32 +1,49 @@
-import datetime,ctypes,time
-x = datetime.datetime.now()
-#CURRENT TIME
-CH=int(x.strftime("%H"))
-CM=int(x.strftime("%M"))
-CS=int(x.strftime("%S"))
-CMonth = int(x.strftime("%m"))
-#Starting Hr Calculation
-if CMonth <=3 or CMonth >=9:
-    startH = 2
-else:
-    startH = 3
-#TIME FROM Starting Hour
-if CH >= startH:
-    calH = CH-startH
-else:
-    calH = CH+24-startH
-upSec = calH*3600+CM*60+CS
-startSec = int(upSec//169.4)
-print(startSec)
+import datetime
+import time
+
+import ctypes
+
+import getpass
+
+
 val = 510
-num = 169.
-n = startSec%510
-while n < int(val)+1:
-    path = "C:\\Users\\amlan\\Desktop\\mojave\\wp"+str(n)+".jpg"
+num = 169.4
+
+currtime = datetime.datetime.now()  # Get the current time
+
+# Separate the *currtime* into different units of time
+CH = int(currtime.strftime("%H"))
+CM = int(currtime.strftime("%M"))
+CS = int(currtime.strftime("%S"))
+CMonth = int(currtime.strftime("%m"))
+
+startH = 2 if CMonth <=3 or CMonth >= 9 else 3
+calH = CH - startH
+
+if CH < startH:
+    calH += 24
+
+upSec = calH * 3600 + CM * 60 + CS
+startSec = int(upSec//num)
+
+print(startSec) # print debugging
+
+n = startSec % val
+username = getpass.getuser()
+num = int(num)
+
+while n <= val:
+    path = f'C:\\Users\\{username}\\Desktop\\WallpaperChanger\\wp{n}.png'
+    
     y = datetime.datetime.now()
-    print(path,y)
-    ctypes.windll.user32.SystemParametersInfoW(20,0,path,0)
-    time.sleep(int(num))
-    n=int(n)+1
-    if n == int(val)+1:
+    
+    print(path, y)  # print debugging
+    
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
+    
+    time.sleep(num)
+    
+    n += 1
+    
+    if n == val + 1:
         n = 1
