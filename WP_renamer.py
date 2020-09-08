43cb5f79e2ecc61cf62ca65024d14aa18ad6c8e4
AVINASH MADDIKONDA <SvasssakAvi@gmail.com>
2020-09-08 18:12:49 +0530

    Update WP_renamer.py
    Modified `path` with `getpass.getuser` method, and modified the parameters of `str.join`

--- WP_renamer.py
+++ WP_renamer.py
@@ -1,7 +1,14 @@
 import os
-path = '/Users/amlan/Desktop/NewFolder'
+
+# Import *getpass* for function *getuser* to get username
+import getpass
+
+path = f'C:\\Users\\{getpass.getuser()}\\Desktop\\WallpaperChanger'
+
+if not os.path.exists(path):    # Create the folder *WallpaperChanger* if it doesn't exist
+    os.makedirs(path)
+
 files = os.listdir(path)
 
-
 for index, file in enumerate(files):
-    os.rename(os.path.join(path, file), os.path.join(path, ''.join(["wp"+str(index), '.jpg'])))
+    os.rename(os.path.join(path, file), os.path.join(path, ''.join(('wp', str(index), '.png'))))
